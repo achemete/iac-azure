2efa1ff17deccd3ef05906d66900adf0da62bba7
achemete <achemete@users.noreply.github.com>
2025-07-13 13:46:00 +0200

    Revert "Dev"

--- dev/acr.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_container_registry" "acr" {
-    name = "terraformacrunir"
-    resource_group_name = azurerm_resource_group.rg-dev.name
-    location = azurerm_resource_group.rg-dev.location
-    sku = "Basic"
-    admin_enabled = true
-}
